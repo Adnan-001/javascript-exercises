7b04c62aafd9bc9633d5eac337de45ec397329e0
Adnan-001 <107484208+Adnan-001@users.noreply.github.com>
2022-07-04 18:08:46 +0500

    Solve exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strArray = Array.from(str);
+    let reversedStr='';
+    for (let i = strArray.length-1; i >= 0; i--) {
+        const element = strArray[i];
+        reversedStr += element;
+    }
+    return reversedStr;
 };
 
 // Do not edit below this line
