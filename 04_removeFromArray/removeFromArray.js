f6a29b2b71e919337ea52c962491801792dafec3
Adnan-001 <107484208+Adnan-001@users.noreply.github.com>
2022-07-04 22:47:12 +0500

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...valsToRemove) {
+    for (let i = 0; i < arr.length; i++) {
 
+        const element = arr[i];
+        for (let j = 0; j < valsToRemove.length; j++) {
+
+            if (element === valsToRemove[j]) 
+            {
+                delete arr[i];
+                break;
+            }    
+        }
+    }
+
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== undefined) {
+            result.push(arr[i]);       
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
