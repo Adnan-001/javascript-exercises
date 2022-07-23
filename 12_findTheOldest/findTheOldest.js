cfd259dcb469eb4bcc75e58fa372f0f104f3e28c
Adnan-001 <107484208+Adnan-001@users.noreply.github.com>
2022-07-23 18:12:42 +0500

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) =>{
 
+        if (!('yearOfDeath' in current)) {
+            currentAge = (new Date()).getFullYear() - current.yearOfBirth;            
+        }
+        else{
+            currentAge = current.yearOfDeath - current.yearOfBirth;
+        }
+
+        if (!('yearOfDeath' in oldest)) {
+
+            oldestAge = (new Date()).getFullYear() - oldest.yearOfBirth;            
+        }
+        else{
+            oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        }        
+
+        if (currentAge > oldestAge) {
+            oldest = current; 
+        }
+        return oldest;
+    });
 };
 
 // Do not edit below this line
