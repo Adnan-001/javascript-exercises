9757ff089af7540d1d1d0e12205e65f43892df33
Adnan-001 <107484208+Adnan-001@users.noreply.github.com>
2022-07-23 17:08:06 +0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(reqSeqNo) {
+    if (reqSeqNo < 0) {
+        return 'OOPS';
+    }
 
+    reqSeqNo = parseInt(reqSeqNo);
+
+    if(reqSeqNo === 1 || reqSeqNo === 2)
+    {
+        return 1;
+    }
+    if (reqSeqNo === 3) {
+        return 2;
+    }
+
+    let currentSeqNo = 4;
+    let prevSeqNo = 1, nextSeqNo = 2, temp;
+
+    while (currentSeqNo !== reqSeqNo) {   
+        temp = prevSeqNo;
+        prevSeqNo = nextSeqNo;
+        nextSeqNo = nextSeqNo + temp;
+
+        currentSeqNo++;
+    }
+    return prevSeqNo+nextSeqNo;
 };
 
 // Do not edit below this line
