de2cdfcd2778a6c77bf7a176879b910c3c694317
Adnan-001 <107484208+Adnan-001@users.noreply.github.com>
2022-07-23 16:02:55 +0500

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,37 @@
-const palindromes = function () {
 
+
+function removeNonAlphabets(string) {
+    let resStr = '';
+    for (let i = 0; i < string.length; i++) {
+        let element = string.charAt(i);
+        if ((element >= 'a' && element <= 'z') 
+            || (element>='A' && element<='Z')) 
+        {
+            element = element.toLowerCase();
+            resStr += element;
+        }
+    }
+    return resStr;
+}
+
+const palindromes = function (string) {
+    const alphabetsList = removeNonAlphabets(string);
+
+    let startInd=0, lastInd = alphabetsList.length-1, startChar, lastChar;
+
+    while (startInd <= lastInd) {
+
+        startChar = alphabetsList[startInd];
+        lastChar = alphabetsList.charAt(lastInd);
+
+        if (startChar !== lastChar) {
+            return false;
+        }
+
+        startInd++;
+        lastInd--;
+    }
+    return true;
 };
 
 // Do not edit below this line
